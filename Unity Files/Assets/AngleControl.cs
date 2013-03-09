792a71be9f3cf6b4cd726844075936fa08e62c03
Matthew Moore <matthewemmoore@googlemail.com>
2013-03-09 18:08:57 +0000

    Updating Angle Control

--- Unity Files/Assets/AngleControl.cs
+++ Unity Files/Assets/AngleControl.cs
@@ -8,6 +8,9 @@ public class AngleControl : MonoBehaviour {
 	public int zRotation = 0;
 	public int rotSpeed = 17;
 
+	public Vector3 forwardForce;
+	public Vector3 reverseForce;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -25,23 +28,63 @@ public class AngleControl : MonoBehaviour {
 		{
 			Down ();
 		}
-	
+		
+		if (Input.GetKey (KeyCode.A))
+		{
+			Left ();
+		}
+		
+		if (Input.GetKey (KeyCode.D))
+		{
+			Right ();
+		}
+		
+		if (Input.GetKey (KeyCode.LeftShift))
+		{
+		}
+		
+		if (Input.GetKey (KeyCode.LeftControl))
+		{
+			rigidbody.AddRelativeForce(reverseForce);
+		}
+		
+			Vector3 nintyMinus = new Vector3 (-90, 0, 0);
+			transform.Translate((transform.forward - nintyMinus) * (Time.deltaTime * 1));
 	}
 		
 		void Up ()
 		{
-			float translation = Time.deltaTime * rotSpeed;
-			//xRotation = xRotation + 1;
-			//Vector3 xRot = new Vector3 (xRotation, yRotation, zRotation);
-			transform.Rotate(Vector3.right, translation);
+			//if ((transform.eulerAngles.x >= 0 && transform.eulerAngles.x <= 45) || transform.eulerAngles.x >= 313) //Works if Greater than 313 aor less than 45
+			//{
+				float translation = Time.deltaTime * rotSpeed;
+				transform.Rotate(Vector3.down, translation);
+			//}
 		}
 		
 		void Down ()
 		{
-			//xRotation = 0;
-			float translation = Time.deltaTime * rotSpeed;
-			//xRotation = xRotation - 1;
-			//Vector3 xRot = new Vector3 (xRotation, yRotation, zRotation);
-			transform.Rotate(Vector3.left, translation);
+			//if ((transform.eulerAngles.x >= 0 && transform.eulerAngles.x <= 47) || transform.eulerAngles.x >= 315) //Works if Greater than 315 or less than 47
+			//{
+				float translation = Time.deltaTime * rotSpeed;
+				transform.Rotate(Vector3.up, translation);
+			//}
+		}
+	
+		void Left ()
+		{
+			//if ((transform.eulerAngles.y >= 0 && transform.eulerAngles.y <= 45) || transform.eulerAngles.y >= 313) //Works if Greater than 315 or less than 47
+			//{
+				float translation = Time.deltaTime * rotSpeed;
+				transform.Rotate(Vector3.right, translation);
+			//}
+		}
+	
+		void Right ()
+		{
+			//if ((transform.eulerAngles.y >= 0 && transform.eulerAngles.y <= 47) || transform.eulerAngles.y >= 315) //Works if Greater than 315 or less than 47
+			//{
+				float translation = Time.deltaTime * rotSpeed;
+				transform.Rotate(Vector3.left, translation);
+			//}
 		}
 }
