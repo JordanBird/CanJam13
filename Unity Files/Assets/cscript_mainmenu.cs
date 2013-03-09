b7948d3215d5618ac43de07065bfc0afce2bfcf9
Jordan Bird <jordanbird-29@hotmail.co.uk>
2013-03-09 12:15:43 +0000

    See What Happens..

---
+++ Unity Files/Assets/cscript_mainmenu.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+
+public class cscript_mainmenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnGUI () {
+		GUI.Label (new Rect (25, 25, 100, 30), "Label");
+	}
+}
